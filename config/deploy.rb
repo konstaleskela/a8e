07fb2cf8051a96a5861a7c28afa83d8e47bd21e4
Nikke <nileskel@gmail.com>
2015-01-26 14:04:37 +0200

    deploy from nles

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :log_level, :info
 set :user, 'deploy'
 set :application, 'a8e'
-set :repo_url, 'https://github.com/konstaleskela/a8e.git'
+set :repo_url, 'https://github.com/nles/a8e.git'
 ask :branch, 'master'
 set :runner, 'deploy'
 set :app_server, :puma
