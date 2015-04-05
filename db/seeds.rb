f0f9804f6b6114ab674177c7d4b4bfaeb7028c3d
Nikke <nileskel@gmail.com>
2015-04-05 18:21:35 +0300

    removed extra schools

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,15 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 schools = [
-  'Katedralskolan i Åbo',
   'Kerttulin lukio',
-  'Luostarivuoren lukio',
   'Puolalanmäen lukio',
   'Turun klassillinen lukio',
   'Turun Lyseon lukio',
-  'Turun Normaalikoulu ja lukio',
-  'Turun Steiner-koulu',
-  'Turun Suomalaisen Yhteiskoulun lukio (TSYK)'
 ]
 schools.each do |name|
   School.create(name: name)
 end
 
 Event.create({
-  :name => "Abit Goes To Tallinn 2016",
+  :name => "Abi Goes Tallinn 2016",
 });
